f2ce53e5d59dac855e3b8b62739b81f38902187e
Dán Imre <dan.imre.17f@szlgbp.hu>
2021-02-10 12:05:37 +0100

    Elso modellek, admin szerkezthet Tanulo

--- elsoprojekt/elsoapp/models.py
+++ elsoprojekt/elsoapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Tanulo(models.Model):
+    nev = models.CharField(max_length=128)
+    felnev = models.CharField(max_length=128)
+    jelszo = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+
+class Foglalkozas(models.Model):
+    nev = models.CharField(max_length=128)
+
+class Valasztas(models.Model):
+    tanulo = models.ForeignKey(Tanulo, on_delete=models.CASCADE)
+    Foglalkozas = models.ForeignKey(Foglalkozas, on_delete=models.CASCADE)
